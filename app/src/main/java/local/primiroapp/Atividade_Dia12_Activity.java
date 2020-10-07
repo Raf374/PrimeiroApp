12b0d5a0d4f5252f71984028ba01295ba4d67d14
Rafael <rafael.i.porto374@gmail.com>
2020-10-07 12:33:14 -0300

    Atividade tela de login

---
+++ app/src/main/java/local/primiroapp/Atividade_Dia12_Activity.java
@@ -0,0 +1,14 @@
+package local.primiroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Atividade_Dia12_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_atividade_dia12_layout);
+    }
+}
