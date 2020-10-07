27d408cfe4bea1bfd3a2b56a9d0100f018801282
Rafael <rafael.i.porto374@gmail.com>
2020-10-07 13:00:54 -0300

    Atividade tela de login 28/09

---
+++ app/src/main/java/local/primiroapp/Atividade_Dia28_Activity.java
@@ -0,0 +1,14 @@
+package local.primiroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Atividade_Dia28_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.atividade__dia28_layout);
+    }
+}
